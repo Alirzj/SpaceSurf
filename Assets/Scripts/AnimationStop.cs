bc4ea7ad4228d481905260125015d2cca06d457e
AdnanZXgamer <aldeeb.adnam@gmail.com>
2025-07-13 18:05:07 +0800

    push

--- Assets/Scripts/AnimationStop.cs
+++ Assets/Scripts/AnimationStop.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class AnimationStop : MonoBehaviour
 {
+    public PowerUp.PowerUpType type; // Set this in the prefab or during spawn
+
     public void DestroySelf()
     {
+        // Deactivate the powerup effect before destroying
+        PlayerPowerUpController player = FindFirstObjectByType<PlayerPowerUpController>();
+        if (player != null)
+        {
+            player.DeactivatePowerUpEffect(type);
+        }
+
         Destroy(gameObject);
     }
-
 }
